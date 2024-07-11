3babdee996404010517f77a37073dda7c4863f01
“Dawitt-dev” <davoza27@yahoo.com>
2024-07-11 22:18:14 +0000

    task_1. In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create the database if it doesn't exist
+-- Create the users table with the specified constraints
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
